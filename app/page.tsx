a2fce01d119ec14b24234e02d8dc21e65f46daf6
Shivkumar-Raghuwanshi <shivkumarraghuwanshi56@gmail.com>
2024-02-02 22:51:13 +0530

    Final Update 2

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,12 @@ export default function Home() {
   return (
     <div className="relative w-full">
       <div className="fixed z-20 md:flex justify-between w-full bg-slate-100 items-center gap-2 px-3 hidden ">
-        <Link href="/documentation" className="hidden md:block">
+        <Link href="/documentation">
           <Button>Documentation</Button>
         </Link>
         <Link
           href={"https://github.com/Shivkumar-Raghuwanshi/search"}
           target="_blank"
-          className="hidden md:block"
         >
           <FaGithub className="w-8 h-10" />
         </Link>
